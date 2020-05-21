fd8b1070dbb1ff11dc04e32c94b362fd905a64e6
Aaron Lee <alee@aaronosaur.us>
2020-05-21 11:29:58 -0500

    Integration test using select.rs

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,9 +1,18 @@
-use crayons::Document;
+use crayons::Document as cDoc;
+use select::document::Document;
+use select::predicate::Name;
 
 #[test]
 fn test_empty_document() {
-    let doc = Document::new();
-    //let res = doc.render();
-    //println!(">> {}", res);
-    assert_eq!(2, 1 + 1);
+    let mut doc = cDoc::new();
+
+    doc.head.title.text = "test".to_owned();
+
+    let html = &doc.render()[..];
+
+    let document = Document::from(html);
+
+    let title = document.find(Name("title")).last().unwrap();
+
+    assert_eq!("test", title.text());
 }
